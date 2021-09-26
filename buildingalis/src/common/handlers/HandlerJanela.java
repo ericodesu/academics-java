328e3d0f7d328673eafbb2c249080a273e174f07
ericodess <erickfcaldeira@gmail.com>
2021-09-26 04:59:43 -0300

    Introduzed a standarized way to sanatize window user input

--- buildingalis/src/common/handlers/HandlerJanela.java
+++ buildingalis/src/common/handlers/HandlerJanela.java
@@ -1,6 +1,8 @@
 package common.handlers;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JTable;
 
 /**
  *
@@ -14,5 +16,25 @@ public class HandlerJanela {
     public void rederizarJanela(JFrame janela) {
         janela.setLocationRelativeTo(null);
         janela.setVisible(true);
+    }
+
+    public void limparListaDeInput(JTextField[] listaInput){
+        for (JTextField jTInput : listaInput) {
+            jTInput.setText("");
+        }
+    }
+
+    public void atulizarListaDeInputRelacionadoTabela(
+            JTextField[] listaInput,
+            JTable jTabela
+    ){
+        if(jTabela.getSelectedRow() != -1 && listaInput.length == jTabela.getColumnCount()){
+            for(int i = 0; i < listaInput.length; i++){
+                JTextField jTInput = listaInput[i];
+                String valorDaColuna = jTabela.getValueAt(jTabela.getSelectedRow(), i).toString();
+
+                jTInput.setText(valorDaColuna);
+            }
+        }
     }
 }
